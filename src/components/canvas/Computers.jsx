8d6a1a2d76b2a21416c8746a754652522297cb11
Rodrigo Yanez <wintermute.ryf@gmail.com>
2023-09-02 20:35:34 -0600

    Setting back mobile configs

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -49,8 +49,7 @@ const ComputersCanvas = () => {
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
         />
-        {/* <Computers isMobile={isMobile}/> */}
-        <Computers />
+        <Computers isMobile={isMobile}/>
       </Suspense>
 
       <Preload all />
