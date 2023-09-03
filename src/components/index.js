c1c17f8811314b24809b427d33b2ac6949f98f3e
Rodrigo Yanez <wintermute.ryf@gmail.com>
2023-09-02 22:48:21 -0600

    testing lazy loading

--- src/components/index.js
+++ src/components/index.js
@@ -1,13 +1,40 @@
 import { RobotCanvas, BallCanvas, ComputersCanvas, StarsCanvas } from './canvas';
-import Hero from './Hero';
-import Navbar from './Navbar';
-import About from './About';
-import Tech from './Tech';
-import Experience from './Experience';
-import Works from './Works';
-import Feedbacks from './Feedbacks';
-import Contact from './Contact';
-import Footer from './Footer';
+// import Hero from './Hero';
+// import Navbar from './Navbar';
+// import About from './About';
+// import Tech from './Tech';
+// import Experience from './Experience';
+// import Works from './Works';
+// import Feedbacks from './Feedbacks';
+// import Contact from './Contact';
+// import Footer from './Footer';
+
+// export {
+//   Hero,
+//   Navbar,
+//   About,
+//   Tech,
+//   Experience,
+//   Works,
+//   Feedbacks,
+//   Contact,
+//   RobotCanvas, 
+//   BallCanvas, 
+//   ComputersCanvas, 
+//   StarsCanvas,
+//   Footer
+// }
+import { lazy } from "react";
+const Hero = lazy(() => import("./Hero"));
+const Navbar = lazy(() => import("./Navbar"));
+const About = lazy(() => import("./About"));
+const Tech = lazy(() => import("./Tech"));
+const Experience = lazy(() => import("./Experience"));
+const Works = lazy(() => import("./Works"));
+const Feedbacks = lazy(() => import("./Feedbacks"));
+const Contact = lazy(() => import("./Contact"));
+const Footer = lazy(() => import("./Footer"));
+
 
 export {
   Hero,
