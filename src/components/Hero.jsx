a11c3f0732cb38c6ed2619a618db48412f0c0a09
Rodrigo Yanez <wintermute.ryf@gmail.com>
2023-09-02 21:02:38 -0600

    Testing diferent canvas

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,6 +3,7 @@ import { motion } from "framer-motion";
 
 import { styles } from "../styles";
 import { ComputersCanvas } from "./canvas";
+import { RobotCanvas } from "./canvas";
 
 const Hero = () => {
   return (
@@ -26,7 +27,8 @@ const Hero = () => {
         </div>
       </div>
       
-      <ComputersCanvas />
+      {/* <ComputersCanvas /> */}
+      <RobotCanvas />
       <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
         <a href="#about">
           <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
