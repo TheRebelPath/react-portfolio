6bf188abe3d60709068afd7e3eeb86e8d52be249
Rodrigo Yanez <wintermute.ryf@gmail.com>
2023-09-02 21:45:01 -0600

    setting canvas back to default

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,6 @@ import { motion } from "framer-motion";
 
 import { styles } from "../styles";
 import { ComputersCanvas } from "./canvas";
-import { RobotCanvas } from "./canvas";
 
 const Hero = () => {
   return (
@@ -27,8 +26,7 @@ const Hero = () => {
         </div>
       </div>
       
-      {/* <ComputersCanvas /> */}
-      <RobotCanvas />
+      <ComputersCanvas />
       <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
         <a href="#about">
           <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
